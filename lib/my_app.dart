649b989e53be6c8fe182a08a33549aca772df25b
Noven Grace <noveei0195@gmail.com>
2024-09-01 15:26:32 +0800

    ✨ feat (myapp): Add my app file

---
+++ lib/my_app.dart
@@ -0,0 +1,16 @@
+import 'package:admin/admin/admin.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const FluentApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: Admin(),
+    );
+  }
+}
